tom-seddon/beeblink

commit e97eb46c6cb7f5b21da3deba9e969afecfdc49f6

Volume revamp, 1/N.
- replace volume paths with a BeebVolume class
- allow volume name to be controlled directly by .volume file
- allow volume not mounted state

server/server.ts
@@ -685,7 +685,7 @@ export class Server {
     }
 
     private async handleGetBootOption(handler: Handler, p: Buffer): Promise<Packet> {
-        const option = await beebfs.BeebFS.loadBootOption(this.bfs.getVolumePath(), this.bfs.getDrive());
+        const option = await beebfs.BeebFS.loadBootOption(this.bfs.getVolume(), this.bfs.getDrive());
 
         return new Packet(beeblink.RESPONSE_BOOT_OPTION, option);
     }
@@ -744,7 +744,7 @@ export class Server {
             const height = p[3];
             const m128 = p[4] >= 3;
 
-            const volumePaths = await this.bfs.findPathsOfVolumesMatching('*');
+            const volumePaths = await this.bfs.findVolumesMatching('*');
 
             this.volumeBrowser = new volumebrowser.Browser(charSizeBytes, width, height, m128, volumePaths);
 
@@ -772,15 +772,9 @@ export class Server {
                     builder.writeBuffer(result.text);
                 }
 
-                if (result.volumePath !== undefined) {
-                    const message = await this.bfs.mountByPath(result.volumePath);
-                    if (message === undefined) {
-                        builder.writeString('New volume: ' + this.bfs.getVolumeName());
-                    } else {
-                        builder.writeString(message);
-                    }
-
-                    builder.writeString(BNL);
+                if (result.volume !== undefined) {
+                    const volume = await this.bfs.mount(result.volume);
+                    builder.writeString('New volume: ' + result.volume.name + BNL);
 
                     if (result.boot) {
                         responseType = beeblink.RESPONSE_VOLUME_BROWSER_BOOT;
@@ -982,21 +976,17 @@ export class Server {
     private async volsCommand(commandLine: beebfs.CommandLine): Promise<Packet> {
         const arg = commandLine.parts.length >= 2 ? commandLine.parts[1] : '*';
 
-        const volumeNames = await this.bfs.findPathsOfVolumesMatching(arg);
+        const volumes = await this.bfs.findVolumesMatching(arg);
 
         let text = 'Matching volumes:';
 
-        if (volumeNames.length === 0) {
+        if (volumes.length === 0) {
             text += ' None';
         } else {
-            for (let i = 0; i < volumeNames.length; ++i) {
-                volumeNames[i] = path.basename(volumeNames[i]);
-            }
+            volumes.sort((a, b) => utils.stricmp(a.name, b.name));
 
-            volumeNames.sort(utils.stricmp);
-
-            for (const volumeName of volumeNames) {
-                text += ' ' + volumeName;
+            for (const volume of volumes) {
+                text += ' ' + volume.name;
             }
 
         }
@@ -1156,16 +1146,16 @@ export class Server {
         const dirRegExp = afsp.dir !== undefined ? utils.getRegExpFromAFSP(afsp.dir) : undefined;
         const nameRegExp = utils.getRegExpFromAFSP(afsp.name);
 
-        const volumePaths = await this.bfs.findPathsOfVolumesMatching('*');
+        const volumes = await this.bfs.findVolumesMatching('*');
         const foundFiles = [];
 
         // This is almost, but not quite, what BeebFS.getBeebFilesForAFSP does.
         // Should probably tidy that up.
 
-        for (const volumePath of volumePaths) {
-            const drives = await beebfs.BeebFS.findDrivesForVolume(volumePath);
+        for (const volume of volumes) {
+            const drives = await beebfs.BeebFS.findDrivesForVolume(volume);
             for (const drive of drives) {
-                const files = await beebfs.BeebFS.getBeebFiles(volumePath, drive.name, undefined);
+                const files = await beebfs.BeebFS.getBeebFiles(volume, drive.name, undefined);
                 for (const file of files) {
                     if ((dirRegExp === undefined || dirRegExp.exec(file.name.dir) !== null) && nameRegExp.exec(file.name.name) !== null) {
                         foundFiles.push(file);
@@ -1179,7 +1169,7 @@ export class Server {
             text += 'No files found.' + utils.BNL;
         } else {
             for (const foundFile of foundFiles) {
-                text += '::' + path.basename(foundFile.name.volumePath) + ':' + foundFile.name.drive + '.' + foundFile.name.dir + '.' + foundFile.name.name + utils.BNL;
+                text += '::' + foundFile.name.volume.name + ':' + foundFile.name.drive + '.' + foundFile.name.dir + '.' + foundFile.name.name + utils.BNL;
             }
         }
 
@@ -1283,13 +1273,16 @@ export class Server {
     }
 
     private async volCommand(commandLine: beebfs.CommandLine): Promise<Packet> {
+        let result: string | beebfs.BeebVolume;
         if (commandLine.parts.length >= 2) {
-            const message = await this.bfs.mountByName(commandLine.parts[1]);
-            if (message !== undefined) {
-                return this.textResponse(message + BNL);
+            result = await this.bfs.mountByName(commandLine.parts[1]);
+            if (typeof (result) === 'string') {
+                return this.textResponse(result + BNL);
             }
+        } else {
+            result = this.bfs.getVolume();
         }
 
-        return this.textResponse('Volume: ' + this.bfs.getVolumeName() + BNL + 'Path: ' + this.bfs.getVolumePath() + BNL);
+        return this.textResponse('Volume: ' + result.name + BNL + 'Path: ' + result.path + BNL);
     }
 }

server/gitattributes.ts
@@ -67,7 +67,7 @@ export class Manipulator {
 
     public scanForBASIC(drive: beebfs.BeebDrive): void {
         this.push(async (): Promise<void> => {
-            const beebFiles = await beebfs.BeebFS.getBeebFiles(drive.volumePath, drive.name, undefined);
+            const beebFiles = await beebfs.BeebFS.getBeebFiles(drive.volume, drive.name, undefined);
 
             //this.log.pn(path.join(drive.volumePath, drive.name) + ': ' + beebFiles.length + ' Beeb file(s)\n');
 

server/main.ts
@@ -332,9 +332,9 @@ class Connection {
     public static async create(options: ICommandLineOptions, connectionId: number, usbSerial: string, colours: Chalk, gaManipulator: gitattributes.Manipulator | undefined): Promise<Connection> {
         const bfs = new beebfs.BeebFS(options.fs_verbose ? 'FS' + connectionId : undefined, options.folders, colours, gaManipulator);
 
-        const mountError = await bfs.mountByName(options.default_volume !== null ? options.default_volume : DEFAULT_VOLUME);
-        if (mountError !== undefined) {
-            throw new Error('Failed to mount initial volume: ' + mountError);
+        const mountResult = await bfs.mountByName(options.default_volume !== null ? options.default_volume : DEFAULT_VOLUME);
+        if (typeof (mountResult) === 'string') {
+            throw new Error('Failed to load initial volume: ' + mountResult);
         }
 
         const server = new Server(options.rom!, bfs, options.server_verbose ? 'SRV' + connectionId : undefined, colours);
@@ -698,7 +698,7 @@ async function main(options: ICommandLineOptions) {
         gaManipulator = new gitattributes.Manipulator(options.git_verbose);
 
         process.stderr.write('Checking for .gitattributes...\n');
-        const volumePaths = await beebfs.BeebFS.findAllVolumePaths(options.folders, log);
+        const volumes = await beebfs.BeebFS.findAllVolumes(options.folders, log);
 
         // Find all the paths first, then set the gitattributes manipulator
         // going once they've all been collected, in the interests of doing one
@@ -707,19 +707,19 @@ async function main(options: ICommandLineOptions) {
 
         const allDrives: beebfs.BeebDrive[] = [];
         let numFolders = 0;
-        for (const volumePath of volumePaths) {
-            const drives = await beebfs.BeebFS.findDrivesForVolume(volumePath);
+        for (const volume of volumes) {
+            const drives = await beebfs.BeebFS.findDrivesForVolume(volume);
             for (const drive of drives) {
                 ++numFolders;
-                if (await isGit(path.join(drive.volumePath, drive.name))) {
+                if (await isGit(path.join(drive.volume.path, drive.name))) {
                     allDrives.push(drive);
                 }
             }
         }
-        process.stderr.write('Found ' + allDrives.length + '/' + numFolders + ' git drive(s) in ' + volumePaths.length + ' volume(s)\n');
+        process.stderr.write('Found ' + allDrives.length + '/' + numFolders + ' git drive(s) in ' + volumes.length + ' volume(s)\n');
 
         for (const drive of allDrives) {
-            const drivePath = path.join(drive.volumePath, drive.name);
+            const drivePath = path.join(drive.volume.path, drive.name);
 
             gaManipulator.makeFolderNotText(drivePath);
             gaManipulator.scanForBASIC(drive);

server/volumebrowser.ts
@@ -22,25 +22,13 @@
 
 import * as path from 'path';
 import * as utils from './utils';
-
-/////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////
-
-class Volume {
-    public readonly volumePath: string;
-    public readonly displayName: string;
-
-    public constructor(volumePath: string) {
-        this.volumePath = volumePath;
-        this.displayName = path.basename(this.volumePath);
-    }
-}
+import * as beebfs from './beebfs';
 
 /////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////
 
 class Column {
-    public rows: Volume[] = [];
+    public rows: beebfs.BeebVolume[] = [];
     public width: number = 0;
     public x: number = 0;
 }
@@ -51,21 +39,15 @@ class Column {
 export class Result {
     public readonly done: boolean;
     public readonly text: Buffer;
-    public readonly volumePath: string | undefined;
+    public readonly volume: beebfs.BeebVolume | undefined;
     public readonly boot: boolean;
     public readonly flushKeyboardBuffer: boolean;
 
-    public constructor(done: boolean, text: Buffer, volume: Volume | undefined, boot: boolean, flushKeyboardBuffer: boolean) {
+    public constructor(done: boolean, text: Buffer, volume: beebfs.BeebVolume | undefined, boot: boolean, flushKeyboardBuffer: boolean) {
         this.done = done;
-
         this.text = text;
-
-        if (volume !== undefined) {
-            this.volumePath = volume.volumePath;
-        }
-
+        this.volume = volume;
         this.boot = boot;
-
         this.flushKeyboardBuffer = flushKeyboardBuffer;
     }
 }
@@ -91,7 +73,7 @@ export class Browser {
     private highlight: string;
     private columns: Column[];
     private numFilteredVolumes: number;
-    private volumes: Volume[];
+    private volumes: beebfs.BeebVolume[];
     private x: number;
     private rowIdx: number;
     private colIdx: number;
@@ -112,24 +94,20 @@ export class Browser {
     private flushKeyboardBuffer!: boolean;
     private prints: utils.BufferBuilder;
     private done: boolean;
-    private selectedVolume: Volume | undefined;
+    private selectedVolume: beebfs.BeebVolume | undefined;
     private boot: boolean;
 
     // used in filter edit mode
     private filterEditY: number;
 
-    public constructor(charSizeBytes: number, width: number, height: number, m128: boolean, volumePaths: string[]) {
+    public constructor(charSizeBytes: number, width: number, height: number, m128: boolean, volumes: beebfs.BeebVolume[]) {
         this.log = new utils.Log('BROWSER', process.stderr, true);
 
         this.width = width;
         this.height = height;
 
-        this.volumes = [];
-        for (const volumePath of volumePaths) {
-            this.volumes.push(new Volume(volumePath));
-        }
-
-        this.volumes.sort((a, b) => utils.stricmp(a.displayName, b.displayName));
+        this.volumes = volumes;
+        this.volumes.sort((a, b) => utils.stricmp(a.name, b.name));
 
         if (charSizeBytes === 32) {
             this.normal = this.createString(17, 128, 17, 7);
@@ -249,7 +227,7 @@ export class Browser {
         return buffer;
     }
 
-    private getSelectedVolume(): Volume | undefined {
+    private getSelectedVolume(): beebfs.BeebVolume | undefined {
         if (this.colIdx >= 0 && this.colIdx < this.columns.length) {
             if (this.rowIdx >= 0 && this.rowIdx < this.columns[this.colIdx].rows.length) {
                 return this.columns[this.colIdx].rows[this.rowIdx];
@@ -259,7 +237,7 @@ export class Browser {
         return undefined;
     }
 
-    private updateColumns(oldVolume: Volume | undefined) {
+    private updateColumns(oldVolume: beebfs.BeebVolume | undefined) {
         //this.log.pn(this.rowIdx + ' ' + this.colIdx + ' ' + this.columns.length);
         // let oldVolume: Volume | undefined;
         // if (this.rowIdx >= 0 && this.colIdx >= 0) {
@@ -272,7 +250,7 @@ export class Browser {
         let x = 0;
         this.columns = [];
         for (const volume of this.volumes) {
-            const displayNameLC = volume.displayName.toLowerCase();
+            const displayNameLC = volume.name.toLowerCase();
 
             let match = true;
             for (const filterLC of this.filterLCs) {
@@ -289,7 +267,7 @@ export class Browser {
                     newColumn.x = x;
                 }
 
-                newColumn.width = Math.max(newColumn.width, volume.displayName.length + this.offset);
+                newColumn.width = Math.max(newColumn.width, volume.name.length + this.offset);
 
                 newColumn.rows.push(volume);
                 ++this.numFilteredVolumes;
@@ -429,7 +407,7 @@ export class Browser {
     }
 
     private printFullPath() {
-        const volumePath = this.columns[this.colIdx].rows[this.rowIdx].volumePath;
+        const volumePath = this.columns[this.colIdx].rows[this.rowIdx].path;
 
         const width = this.width - 4;
         const lines = [];
@@ -477,7 +455,7 @@ export class Browser {
     private printPath(colIdx: number, rowIdx: number, highlight: boolean | undefined): void {
         const column = this.columns[colIdx];
 
-        let text = column.rows[rowIdx].displayName;
+        let text = column.rows[rowIdx].name;
 
         let offset = 0;
         if (this.teletext) {

commit cafc7eb371a611debef30d273d9cd31a8810eca8

Fix *NEWVOL.
Probably fallout from e97eb46c6cb7f5b21da3deba9e969afecfdc49f6, as this
used to work...

server/beebfs.ts
@@ -1137,7 +1137,7 @@ export class BeebFS {
         }
 
         // This check is a bit crude, but should catch obvious problems...
-        const volumePath = path.join(this.folders[0], name, '0');
+        const volumePath = path.join(this.folders[0], name);
         try {
             const stat0 = await utils.fsStat(path.join(volumePath, '0'));
             if (stat0.isDirectory()) {
@@ -1162,8 +1162,6 @@ export class BeebFS {
         }
 
         const newVolume = new BeebVolume(volumePath, name);
-        await this.mount(newVolume);
-
         return newVolume;
     }
 

server/server.ts
@@ -1263,8 +1263,11 @@ export class Server {
             throw new CommandSyntaxError();
         }
 
-        const result = await this.bfs.createVolume(commandLine.parts[1]);
-        return this.textResponse(result + BNL);
+        const volume = await this.bfs.createVolume(commandLine.parts[1]);
+
+        await this.bfs.mount(volume);
+
+        return this.textResponse('New volume: ' + volume.name + BNL + 'Path: ' + volume.path + BNL);
     }
 
     private async volCommand(commandLine: beebfs.CommandLine): Promise<Packet> {